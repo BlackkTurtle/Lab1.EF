075033c288572c3119336df5bb7f992b0179b60c
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-04-24 23:18:21 +0300

    Initial commit

---
+++ Lab1.EF/DBContext/DBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1.EF.Entities;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace Lab1.EF.DataBaseContext
+{
+    public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext>option):base(option)
+        {
+
+        }
+        public DbSet<Table1> table1 { get; set; }
+        public DbSet<Table2> table2 { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Table2>()
+                .HasOne(t => t.Table1)
+                .WithMany()
+                .HasForeignKey(t => t.Table1Id);
+        }
+    }
+}
